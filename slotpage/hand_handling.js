01691da61551dd106d5955124ff12610d8e4102a
유민수 <minsoo0515@naver.com>
2025-02-26 14:19:50 +0900

    feat : 배팅 시도 관련 코드 추가 및 주석 추가

--- slotpage/hand_handling.js
+++ slotpage/hand_handling.js
@@ -1,4 +1,4 @@
-// script.js
+// hand_handling.js
 document.addEventListener('DOMContentLoaded', () => {
     const toggle = document.getElementById('toggle');
     const reel1 = document.querySelector('.reel1');
@@ -19,9 +19,22 @@ document.addEventListener('DOMContentLoaded', () => {
     let isSpinning = false;
 
     function spinReels() {
+        // 배팅 시도 관련 코드
+        // placeBet 함수가 false(or return) 반환 시 스핀 취소
+        if(typeof placeBet === 'function') {
+            // TODO : amount 함수에 상수가 아닌 플레이어 입력값 넣어야함
+            const betSuccuss = placeBet(10000); // 예시 : 10000
+            if (!betSuccuss) {
+                 // 배팅 실패시 스핀 중단(배팅 금액 부족 등)
+                return;
+            }
+        }
+
+        // 이미 스핀 중일 경우 취소
         if (isSpinning) return;
         isSpinning = true;
 
+        // 릴 관련 애니메이션
         reel1.classList.add('spinning');
         reel2.classList.add('spinning');
         reel3.classList.add('spinning');
@@ -35,6 +48,12 @@ document.addEventListener('DOMContentLoaded', () => {
         setTimeout(() => {
             reel3.classList.remove('spinning');
             isSpinning = false;
+
+            // 스핀 결과 처리
+            if (typeof spinResult === 'function') {
+                // TODO : slot_db.js 의 spinResult 함수 확률에 따른 가중치 추가 필요
+                spinResult();
+            }
         }, stopTime3);
     }
 

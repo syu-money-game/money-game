7ee933299280d453fee8614a51ecb4f934cc7803
minseong <minseong0213@naver.com>
2025-02-27 09:54:28 +0900

    feat: Reduce health when observer detects lever activation

--- slotpage/lever_handling.js
+++ slotpage/lever_handling.js
@@ -5,10 +5,14 @@ document.addEventListener("DOMContentLoaded", () => {
     function detectLeverActivation() {
         const randomChance = Math.random();
 
-        // 10% 확률로 감시자가 확률조작버튼을 감지함.
+        // 60% 확률로 감시자가 확률 조작 버튼을 감지함.
         if (randomChance < 0.6) {
             console.log("Observer detected the lever!");
             observer.src = "img/observer_active.png";
+
+            // 체력 감소 함수 호출
+            decreaseHealth();
+
             setTimeout(() => {
                 observer.src = "img/observer.png";
             }, 2000);

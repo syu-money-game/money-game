50772d7a88429c1fc84272af17dbe454889e0719
soyoung2222 <145244672+soyoung2222@users.noreply.github.com>
2025-02-27 14:02:09 +0900

    feat: 시작,인게임,종료 시 배경음 설정

--- gameoverpage/restart.js
+++ gameoverpage/restart.js
@@ -1,5 +1,27 @@
+let gameOverBg;
+
+document.addEventListener("DOMContentLoaded", function () {
+    gameOverBg = document.getElementById("gameOverBg");
+    let shouldPlayGameOverBg = sessionStorage.getItem("playGameOverBg") === "true";
+
+    if (shouldPlayGameOverBg) {
+        gameOverBg.volume = 1.0;
+        gameOverBg.play().then(() => {
+            console.log("배경 재생 성공");
+        }).catch(error => {
+            console.error("실패:", error);
+        });
+    }
+
+    sessionStorage.removeItem("playGameOverBg");
+});
+
 document.addEventListener('keydown', function(event) {
     if (event.key === 'Enter') {
-        window.location.href = '../startpage/title.html';  // Navigate to title.html
+        gameOverBg.pause();
+        gameOverBg.currentTime = 0;
+
+        window.location.href = '../startpage/title.html';
     }
-});
+}, { once: true }); // 한번만 실행되도록 설정
+
